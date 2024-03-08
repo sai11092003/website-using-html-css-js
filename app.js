9839559b6fce0344be37d9d4ceba4d8014fbb0d7
sai11092003 <115882637+sai11092003@users.noreply.github.com>
2024-03-08 14:58:42 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,130 +1,47 @@
 
-const wrapper=document.querySelector(".sliderWrapper");
-const menuitems=document.querySelectorAll(".menuitem");
-const cc=document.querySelectorAll(".color");
-const ss=document.querySelectorAll(".size");
-const close=document.querySelector(".close");
-const pp=document.querySelector(".productButton");
-const pay=document.querySelector(".payment");
-
-const product=[
-    {
-        id:1,
-        title:"Air Force",
-        price:119,
-        colors:[
-            {
-                code:"black",
-                img:"air.png"
-            },
-            {
-                code:"darkblue",
-                img:"air2.png",
-            },
-        ],
-    },
-    {
-        id: 2,
-        title: "Air Jordan",
-        price: 149,
-        colors: [
-          {
-            code: "lightgray",
-            img: "jordan.png",
-          },
-          {
-            code: "green",
-            img: "jordan2.png",
-          },
-        ],
-      },
-      {
-        id: 3,
-        title: "Blazer",
-        price: 109,
-        colors: [
-          {
-            code: "lightgray",
-            img: "blazer.png",
-          },
-          {
-            code: "green",
-            img: "blazer2.png",
-          },
-        ],
-      },
-      {
-        id: 4,
-        title: "Crater",
-        price: 129,
-        colors: [
-          {
-            code: "black",
-            img: "crater.png",
-          },
-          {
-            code: "lightgray",
-            img: "crater2.png",
-          },
-        ],
-      },
-      {
-        id: 5,
-        title: "Hippie",
-        price: 99,
-        colors: [
-          {
-            code: "gray",
-            img: "hippie.png",
-          },
-          {
-            code: "black",
-            img: "hippie2.png",
-          },
-        ],
-      },
-];
-let choosen_product=product[0];
-const crntproducttitle=document.querySelector(".productTitle")
-const crntproductimg=document.querySelector(".productImg")
-const crntproductPrice=document.querySelector(".productPrice")
-const crntproductcolor=document.querySelectorAll(".color")
-const crntproductsizes=document.querySelectorAll(".size")
-
-menuitems.forEach((item,index) => {
-    item.addEventListener("click",() => {
-        wrapper.style.transform=`translateX(${index*-100}vw)`;
-        choosen_product=product[index];
-        crntproductimg.src=choosen_product.colors[0].img;
-        crntproducttitle.textContent=choosen_product.title;
-        crntproductPrice.textContent="$"+choosen_product.price;
-        crntproductcolor.forEach((color,ind)=>{
-            color.style.backgroundColor=choosen_product.colors[ind].code;
-        })
-
-    });
-});
-cc.forEach((ii,index)=>{
-ii.addEventListener("click",()=>{
-    crntproductimg.src=choosen_product.colors[index].img;
-});
-});
-ss.forEach((size,index)=>{
-    size.addEventListener("click",()=>{
-        crntproductsizes.forEach((size)=>{
-                size.style.backgroundColor = "white";
-                size.style.color = "black";
-        });
-            size.style.backgroundColor = "black";
-            size.style.color = "white";
-    });
-});
-pay.style.display="none";
-pp.addEventListener("click",()=>
+let starttime=0;
+let elapsedtime=0;
+let timer=null;
+let isrunning=false;
+const display=document.querySelector(".title1");
+function start()
 {
-    pay.style.display="flex";
-});
-close.addEventListener("click",()=>
+    if(!isrunning)
+    {
+        starttime=Date.now()-elapsedtime;
+        timer=setInterval(updatenow,10);
+        isrunning=true;
+    }
+}
+function stop()
 {
-    pay.style.display="none";
-});
+if(isrunning)
+{
+    clearInterval(timer);
+    elapsedtime=Date.now()-starttime;
+    isrunning=false;
+}
+}
+function reset()
+{
+clearInterval(timer);
+elapsedtime=0;
+starttime=0;
+timer=null;
+isrunning=false;
+display.textContent="00:00:00:00";
+}
+function updatenow()
+{
+    const currenttime=Date.now()
+    elapsedtime=currenttime-starttime;
+    let hrs=Math.floor(elapsedtime/(1000*60*60));
+    let mins=Math.floor(elapsedtime/(1000*60)%60);
+    let seconds=Math.floor(elapsedtime/(1000)%60);
+    let mill=Math.floor(elapsedtime%1000/10);
+    hrs=String(hrs).padStart(2,"0");
+    mins=String(mins).padStart(2,"0");
+    seconds=String(seconds).padStart(2,"0");
+    mill=String(mill).padStart(2,"0");
+    display.textContent=`${hrs}:${mins}:${seconds}:${mill}`;
+}
